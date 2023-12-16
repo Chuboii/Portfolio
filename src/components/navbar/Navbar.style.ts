6fe4ae7282bbd7d7760e72d9232ce9d32ac28c9d
Chuboii <favoronyechere@gmail.com>
2023-12-16 03:57:00 +0100

    initial commit

---
+++ src/components/navbar/Navbar.style.ts
@@ -0,0 +1,124 @@
+import styled from 'styled-components'
+
+interface HeaderProps {
+    changebg: string
+}
+
+interface LinkWrapProps {
+    display: string
+}
+
+export const DisplayMenu = styled.div`
+display: block;
+   @media screen and (min-width: 768px) {
+     display:none;
+   }
+`
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+`
+export const Header = styled.header<HeaderProps>`
+  display: flex;
+  justify-content: space-between;
+  align-items:  center;
+  position: fixed;
+  top: 0;
+  left: 0;
+ right: 0;
+  padding:1rem 3rem;
+   background-color: ${({ changebg }) => changebg};
+
+`
+
+export const Logo = styled.h2`
+  display: flex;
+  @media screen and (max-width: 768px) {
+     display:none;
+   }
+`
+
+export const Button = styled.button`
+  display: flex;
+  border: 1px solid white;
+  background-color: transparent;
+`
+
+export const LinkWrap = styled.div<LinkWrapProps>`
+  display: flex;
+  margin-right: 1.5rem;
+
+  @media screen and (max-width:768px){
+    position: absolute;
+    left:0;
+    flex-direction: column;
+    top: 3rem;
+    padding:1rem;
+    right: 2rem;
+    opacity: ${({ display }) => display};
+    transition: all .6s;
+    z-index: 1;
+   }
+`
+
+export const IconWrap = styled.div`
+  display: flex;
+   margin-right: 1rem;
+
+   @media screen and (max-width:768px){
+    position: absolute;
+    left: 2rem;
+    flex-direction: column;
+    top: 13rem;
+      
+   }
+`
+
+export const Link = styled.a`
+  display: flex;
+  align-items: center;
+  margin-right: 2rem;
+  color:   rgba(255, 255, 255,.7);
+  
+ @media screen and (max-width:768px){
+    margin-top: 1rem;
+    background: rgba( 255, 255, 255, 0.25 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 4px );
+-webkit-backdrop-filter: blur( 4px );
+border-radius: 10px;
+border: 1px solid rgba( 255, 255, 255, 0.18 );
+padding: 1rem;
+
+    display: block;
+    width: 100%;
+
+    &:nth-of-type(2){
+       width: 80%;
+    }
+   
+    &:nth-of-type(3){
+       width: 60%;
+    }
+   
+   }
+`
+
+export const Icon = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: .5rem;
+  width: 30px;
+  cursor: pointer;
+  height: 30px;
+  border-radius: 50%;
+  border: 1px solid rgba(255, 255, 255,.7);
+   color:  rgba(255, 255, 255,.7);
+   justify-content: center;
+
+ @media screen and (max-width:768px){
+    margin-top: 2rem;
+   
+   }
+`
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,100 @@
+import { DisplayMenu, Wrapper, Header, Logo, Button, LinkWrap, IconWrap, Link, Icon } from './Navbar.style';
+import React,{ useEffect, useState, useCallback, useReducer } from 'react'
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+
+interface NavbarReducer {
+    isPageScrolled: boolean;
+    isToggled: boolean;
+}
+
+const TOGGLE_INITIAL_STATES : NavbarReducer= {
+    isPageScrolled: false,
+    isToggled: false,   
+}
+
+interface stateObject {
+    isToggled: boolean;
+    isPageScrolled: boolean;
+}
+
+interface actionObject {
+    type: string;
+    payload: boolean;
+}
+
+
+
+const toggleReducer : React.Reducer<stateObject, actionObject> = (state, action) => {
+    const { type, payload } = action
+    
+    switch (type) {
+        case "PAGE_SCROLLED":
+            return { ...state, isPageScrolled: payload }
+        case "TOGGLE_NAVBAR":
+            return { ...state, isToggled: payload }
+        case "INSERT_TEXT":
+                return { ...state, text: payload }
+        default:
+            return state
+    }
+}
+
+
+export default function Navbar() {
+    const [state, dispatch] = useReducer(toggleReducer, TOGGLE_INITIAL_STATES)
+
+
+    useEffect(() => {
+
+        const scrollFunction = () => {
+            const scrollY = window.scrollY
+        
+            if (scrollY > 50) {
+                dispatch({ type: "PAGE_SCROLLED", payload: true })
+            }
+            else {
+                dispatch({ type: "PAGE_SCROLLED", payload: false })
+  
+            }
+        }
+
+        window.addEventListener("scroll", scrollFunction)
+  
+    return () => {
+     window.removeEventListener("scroll", scrollFunction)
+  
+    }
+  }, [state.isPageScrolled])
+  
+  const enableNavBar = useCallback(() => {
+      dispatch({ type: 'TOGGLE_NAVBAR', payload: !state.isToggled })
+  }, [state.isToggled])
+
+
+    return (
+      <>
+            <Header changebg={state.isPageScrolled ? "#242424" : "transparent"}>
+                <Logo>Chuboi</Logo>
+                <DisplayMenu>
+                    <MenuOutlinedIcon onClick={enableNavBar } />
+                </DisplayMenu>
+                <Wrapper>
+                    <LinkWrap display={state.isToggled ? "1" : '0'}>
+                        <Link className={state.isToggled ?"animate__animated animate__backInLeft" : "animate__animated animate__rubberBand"} href="#">Home</Link>
+                        <Link className={state.isToggled ?"animate__animated animate__backInLeft" : "animate__animated animate__rubberBand"}  href="#">Skills</Link>
+                        <Link className={state.isToggled ?"animate__animated animate__backInLeft" : "animate__animated animate__rubberBand"}  href="#">Projects</Link>
+                    </LinkWrap>
+
+                    <IconWrap>
+                     <Icon><i className="fa fa-facebook" aria-hidden="true"></i></Icon>
+                     <Icon><i className="fa fa-linkedin" aria-hidden="true"></i></Icon>
+                     <Icon><i className="fa fa-github" aria-hidden="true"></i></Icon>   
+                        <Icon><i className="fa fa-instagram" aria-hidden="true"></i></Icon>   
+                    </IconWrap>
+
+                    <Button>Let's Connect</Button>
+                </Wrapper>
+    </Header>            
+            </>
+  );
+}
