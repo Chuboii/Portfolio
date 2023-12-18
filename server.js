e2af3169608dfe953c655bb9d3546cf77cd246df
Chuboii <favoronyechere@gmail.com>
2023-12-18 18:33:31 +0100

    added a server for the contact form

---
+++ server.js
@@ -0,0 +1,68 @@
+import express from 'express';
+import nodemailer from 'nodemailer'
+import cors from 'cors'
+import dotenv from 'dotenv'
+const app = express()
+
+
+app.use(express.json())
+dotenv.config()
+
+const router = express.Router()
+
+
+app.use(cors())
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.SECRET_KEY
+        
+    }
+})
+
+
+router.post('/contact', (req, res) => {
+    const { email, firstname,lastname, message,phone } = req.body
+ 
+    try {
+        
+        const mailOptions = {
+            from: process.env.EMAIL,
+            to: email,
+            subject: "Contact Form Submission ~ Portfolio",
+            html: `
+            <h1> Here's the contact information of user: </h1>
+            
+            <p>Firstname: ${firstname} </p>
+            <p>Lastname: ${lastname} </p>
+            <p>Phone: ${phone} </p>
+            <p>Email: ${email} </p>
+            <p>Message: ${message} </p>
+            `
+        }
+
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.error('Error sending email:', error);
+                res.status(500).json("Error sending email")
+            } else {
+                res.status(200).send("email sent successfully")
+                console.log('Email sent:', info.response);
+            }
+        });
+    }
+    catch (e) {
+        console.log(e)
+        res.status(500).json("internal server error")
+    }
+})
+
+app.use(`/api`, router)
+
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+    console.log("listening on port 8080")
+})
--- src/components/contact form/ContactForm.tsx
+++ src/components/contact form/ContactForm.tsx
@@ -1,32 +1,181 @@
-import { FC } from "react";
+import { FC, useState, memo, useEffect } from "react";
 import {Form, Button, H1, Wrapper, Box, Input, Textarea} from './ContactForm.style'
+import axios from 'axios';
+import { useForm } from 'react-hook-form'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
+const ContactForm: FC = memo (() => {
+    const { register, handleSubmit, formState: { errors } } = useForm({ mode: "onChange" })
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
 
 
-const ContactForm: FC = () => {
+    const registerOptions = {
+        firstname: {
+            required: true,
+        },
+        lastname: {
+            required: true,
+        },
+        email: {
+            required: true,
+        },
+        phone: {
+            required: true,
+        },
+        message: {
+            required:true
+        }
+
+
+    }
+
+    useEffect(() => {
+        if (errors.firstname) {
+          toast.error('Your firstname must be provided!', {
+            position: 'top-right',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: 'colored',
+          })
+        }
+        else if (errors.lastname) {
+            toast.error('Your lastname must be provided!', {
+                position: 'top-right',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'colored',
+              }) 
+        }
+        else if (errors.email) {
+            toast.error('Your email must be provided!', {
+                position: 'top-right',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'colored',
+              }) 
+        }
+        else if (errors.phone) {
+            toast.error('Your phone number must be provided!', {
+                position: 'top-right',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'colored',
+              }) 
+        }
+        else if (errors.message) {
+            toast.error('Message must be provided!', {
+                position: 'top-right',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: 'colored',
+              }) 
+        }
+      
+    }, [errors.firstname, errors.lastname, errors.email, errors.phone, errors.message]);
+
+
+    const submitForm = async (data) => {
+        try {
+            setLoading(true)
+            if (data.firstname && data.lastname && data.email && data.phone && data.message) {
+                const res = await axios.post("http://localhost:8080/api/contact", {
+                    firstname: data.firstname,
+                    lastname: data.lastname,
+                    email: data.email,
+                    phone: data.phone,
+                    message: data.message
+                })
     
+                toast.success('Email sent successfully!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                });
+                
+                setLoading(false)
+            }
+            else {
+                toast.error('You must fill all fields!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                });
+            }
+        }
+        catch (e) {
+            setLoading(false)
 
+            if (e.response.data === "Error sending email") {
+             toast.error('Please provide a verified email!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                });
+            }
+           
+        }
+    }
     return (
         <>
-            <Form>
+            <ToastContainer/>
+            <Form onSubmit={handleSubmit(submitForm)}>
                 <H1>Get in Touch</H1>
                 <Wrapper>
                     <Box>
-                        <Input  placeholder="first name" name="first name"/>
-                        <Input placeholder="last name" name="last name"/>
+                        <Input placeholder="first name" name="firstname" {...register("firstname", registerOptions.firstname)} />
+        
+                        <Input placeholder="last name" name="lastname"  {...register("lastname", registerOptions.lastname)} />
                     </Box>
                     <Box>
-                        <Input placeholder="email address" name="email address"/>
-                        <Input placeholder="phone no" name="phone no"/>
+                        <Input placeholder="email address" name="email"  {...register("email", registerOptions.email)}/>
+                        <Input placeholder="phone no" name="phone"  {...register("phone", registerOptions.phone)}/>
                     </Box>
                     <Box>
-                        <Textarea placeholder="message" name="message"/>
+                        <Textarea placeholder="message" name="message"  {...register("message", registerOptions.message)}/>
                     </Box>
 
-                    <Button>Send</Button>
+                    <Button>{loading ? "Sending" : "Send"} </Button>
                 </Wrapper>
         </Form>
         </>
     )
-}
+})
 
 export default ContactForm
